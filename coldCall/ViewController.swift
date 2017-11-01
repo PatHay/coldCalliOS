26a555f4685061c7b56513bf340720e6bcd17796
Patrick Hayes <phayes2828@gmail.com>
2017-10-31 19:17:26 -0700

    assignment completed

--- coldCall/ViewController.swift
+++ coldCall/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+
+    let nameBank: [String] = [
+    "Chuck", "Rodrigo", "Pat", "Elva"
+    ]
+    
+    var random = Int(arc4random_uniform(UInt32(5)))
+    
+    @IBAction func coldCallButton(_ sender: UIButton) {
+        updateUI()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +32,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func updateUI(){
+        nameLabel.text = nameBank[random]
+        random = Int(arc4random_uniform(UInt32(4)))
+    }
+    
+    
 }
 
